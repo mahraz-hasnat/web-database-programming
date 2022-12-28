2721a3b0f65d7290d86a673f43ecf325833eee9a
Abul Hasnat Mahraz <93318140+mahraz-hasnat@users.noreply.github.com>
2022-12-28 18:12:18 +0600

    Add files via upload

---
+++ insertdata.php
@@ -0,0 +1,20 @@
+<?php
+    include "dbconnect.php"
+    $fname=$_POST['fname'];
+    $mname=$_POST['mname'];
+    $lname=$_POST['lname'];
+    $email=$_POST['email'];
+    $pass=$_POST['pass'];
+    $url=$_POST['url'];
+    $dob=$_POST['dob'];
+    $gender=$_POST['gender'];
+    $phn=$_POST['phnNo'];
+    $info=$_POST['info'];
+ 
+    $sql=mysqli_query($conn,"INSERT into user(fname,mname,lname,email,pass,dob,gender,phnNo,info) VALUES('$fname','$mname','$lname','$email','$pass','$url','$dob','$gender','$phn','$info')");
+    
+    header('Location: success.html');
+    exit;
+
+    
+?>
